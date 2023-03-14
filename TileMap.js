c95e803c1f71025094ed0a0c7b64b93fe3263c92
ronaldpannel <69686310+ronaldpannel@users.noreply.github.com>
2023-03-14 11:34:21 +0000

    Add files via upload

--- TileMap.js
+++ TileMap.js
@@ -7,13 +7,13 @@ export default class TileMap {
     this.tileSize = tileSize;
 
     this.yellowDot = new Image();
-    this.yellowDot.src = "images/yellowDot.png";
+    this.yellowDot.src = "yellowDot.png";
 
     this.pinkDot = new Image();
-    this.pinkDot.src = "images/pinkDot.png";
+    this.pinkDot.src = "pinkDot.png";
 
     this.wall = new Image();
-    this.wall.src = "images/wall.png";
+    this.wall.src = "wall.png";
 
     this.powerDot = this.pinkDot;
     this.powerDotAnmationTimerDefault = 30;
